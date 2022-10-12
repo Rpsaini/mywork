d534a94524bc3e470db3c5ac5131ce140a517d7e
Rahul <rahul@signitysolutions.in>
2022-10-12 22:27:59 +0530

    chnages in forgot password and some commebnting lines

--- app/src/main/java/com/markrap/login/ForgotPassword.java
+++ app/src/main/java/com/markrap/login/ForgotPassword.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.app.preferences.SavePreferences;
 import com.markrap.R;
@@ -22,7 +24,8 @@ import java.util.Map;
 
 public class ForgotPassword extends BaseActivity implements View.OnClickListener {
     Button btnForgot;
-
+EditText edit_forgot_password;
+ImageButton back_btn;
     public static Intent getIntent(Context context) {
         return new Intent(context, ForgotPassword.class);
     }
@@ -33,8 +36,11 @@ public class ForgotPassword extends BaseActivity implements View.OnClickListener
     }
 
     private void init() {
+        back_btn=(ImageButton)findViewById(R.id.back_btn);
+        edit_forgot_password=(EditText)findViewById(R.id.edit_forgot_password);
         btnForgot= (Button) findViewById(R.id.btnForgot);
         btnForgot.setOnClickListener(this);
+        back_btn.setOnClickListener(this);
     }
 
     @Override
@@ -44,8 +50,13 @@ public class ForgotPassword extends BaseActivity implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
+        if(v==back_btn){
+
+
+        }
         if (v == btnForgot) {
            // startActivity(Signup.getIntent(ForgotPassword.this));
+            forgotPassword(edit_forgot_password.getText().toString());
         }
 
 
@@ -62,6 +73,7 @@ public class ForgotPassword extends BaseActivity implements View.OnClickListener
             @Override
             public void getRespone(String dta, ArrayList<Object> respons) {
                 try {
+                    System.out.println("forgotPassword===="+AppConstants.apiUlr+"forgotPassword"+m);
                     System.out.println("forgotPassword===="+dta);
                     JSONObject obj = new JSONObject(dta);
                     if (obj.getInt("result")>0)
